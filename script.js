41c1b5778f11a451913258ac322fe1eb7c77c6bc
jchaselubitz <jchaselubitz@gmail.com>
2023-11-24 11:48:21 +0100

    update stuff

--- script.js
+++ script.js
@@ -1,139 +1,155 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = "https://taxifare.lewagon.com"; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
-if (window.location.href.includes('https://taxifare.lewagon.com')) {
-  taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
+if (window.location.href.includes("https://taxifare.lewagon.com")) {
+  taxiFareApiUrl = "https://taxifare.lewagon.ai/predict";
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ";
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
     center: centralCoordinates,
-    zoom: 10 // starting zoom
+    zoom: 10, // starting zoom
   });
 
   function getRoute(end) {
-    var url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+    var url =
+      "https://api.mapbox.com/directions/v5/mapbox/driving/" +
+      start[0] +
+      "," +
+      start[1] +
+      ";" +
+      end[0] +
+      "," +
+      end[1] +
+      "?steps=true&geometries=geojson&access_token=" +
+      mapboxgl.accessToken;
 
     var req = new XMLHttpRequest();
-    req.open('GET', url, true);
-    req.onload = function() {
+    req.open("GET", url, true);
+    req.onload = function () {
       var json = JSON.parse(req.response);
       var data = json.routes[0];
       var route = data.geometry.coordinates;
       var geojson = {
-        type: 'Feature',
+        type: "Feature",
         properties: {},
         geometry: {
-          type: 'LineString',
-          coordinates: route
-        }
+          type: "LineString",
+          coordinates: route,
+        },
       };
       // if the route already exists on the map, reset it using setData
-      if (map.getSource('route')) {
-        map.getSource('route').setData(geojson);
-      } else { // otherwise, make a new request
+      if (map.getSource("route")) {
+        map.getSource("route").setData(geojson);
+      } else {
+        // otherwise, make a new request
         map.addLayer({
-          id: 'route',
-          type: 'line',
+          id: "route",
+          type: "line",
           source: {
-            type: 'geojson',
+            type: "geojson",
             data: {
-              type: 'Feature',
+              type: "Feature",
               properties: {},
               geometry: {
-                type: 'LineString',
-                coordinates: geojson
-              }
-            }
+                type: "LineString",
+                coordinates: geojson,
+              },
+            },
           },
           layout: {
-            'line-join': 'round',
-            'line-cap': 'round'
+            "line-join": "round",
+            "line-cap": "round",
           },
           paint: {
-            'line-color': '#3887be',
-            'line-width': 5,
-            'line-opacity': 0.75
-          }
+            "line-color": "#3887be",
+            "line-width": 5,
+            "line-opacity": 0.75,
+          },
         });
       }
     };
     req.send();
   }
   if (start && stop) {
-    map.on('load', function() {
+    map.on("load", function () {
       // make an initial directions request that
       // starts and ends at the same location
       getRoute(start);
       // Add starting point to the map
       map.addLayer({
-        id: 'point',
-        type: 'circle',
+        id: "point",
+        type: "circle",
         source: {
-          type: 'geojson',
+          type: "geojson",
           data: {
-            type: 'FeatureCollection',
-            features: [{
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: start
-              }
-            }
-            ]
-          }
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: start,
+                },
+              },
+            ],
+          },
         },
         paint: {
-          'circle-radius': 10,
-          'circle-color': '#3887be'
-        }
+          "circle-radius": 10,
+          "circle-color": "#3887be",
+        },
       });
       // this is where the code from the next step will go
-      var coords = stop
+      var coords = stop;
       var end = {
-          type: 'FeatureCollection',
-          features: [{
-            type: 'Feature',
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
             properties: {},
             geometry: {
-              type: 'Point',
-              coordinates: coords
-            }
-          }
-          ]
-        };
-        if (map.getLayer('end')) {
-          map.getSource('end').setData(end);
-        } else {
-          map.addLayer({
-            id: 'end',
-            type: 'circle',
-            source: {
-              type: 'geojson',
-              data: {
-                type: 'FeatureCollection',
-                features: [{
-                  type: 'Feature',
+              type: "Point",
+              coordinates: coords,
+            },
+          },
+        ],
+      };
+      if (map.getLayer("end")) {
+        map.getSource("end").setData(end);
+      } else {
+        map.addLayer({
+          id: "end",
+          type: "circle",
+          source: {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: [
+                {
+                  type: "Feature",
                   properties: {},
                   geometry: {
-                    type: 'Point',
-                    coordinates: coords
-                  }
-                }]
-              }
+                    type: "Point",
+                    coordinates: coords,
+                  },
+                },
+              ],
             },
-            paint: {
-              'circle-radius': 10,
-              'circle-color': '#f30'
-            }
-          });
-        }
-        getRoute(coords);
+          },
+          paint: {
+            "circle-radius": 10,
+            "circle-color": "#f30",
+          },
+        });
+      }
+      getRoute(coords);
     });
   }
 };
@@ -142,33 +158,37 @@ const initGeocoder = (element, placeholder) => {
   const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     placeholder: placeholder,
-    proximity: { // focus on NYC
+    proximity: {
+      // focus on NYC
       longitude: centralCoordinates[0],
-      latitude: centralCoordinates[1]
+      latitude: centralCoordinates[1],
     },
-    countries: 'us'
+    countries: "us",
   });
   geocoder.addTo(element);
   return geocoder;
-}
+};
 
 const pickupAutocomplete = () => {
-  const geocoder = initGeocoder('#pickup', 'Pickup');
-  geocoder.on("result", event => {
-    const coordinates = event['result']['center'];
-    document.querySelector('#pickup_latitude').value = coordinates[1];
-    document.querySelector('#pickup_longitude').value = coordinates[0];
+  const geocoder = initGeocoder("#pickup", "Pickup");
+  geocoder.on("result", (event) => {
+    const coordinates = event["result"]["center"];
+    document.querySelector("#pickup_latitude").value = coordinates[1];
+    document.querySelector("#pickup_longitude").value = coordinates[0];
   });
 };
 
 const dropoffAutocomplete = () => {
-  const geocoder = initGeocoder('#dropoff', 'Drop-off');
-  geocoder.on("result", event => {
-    const coordinates = event['result']['center'];
-    document.querySelector('#dropoff_latitude').value = coordinates[1];
-    document.querySelector('#dropoff_longitude').value = coordinates[0];
-    const start = [parseFloat(document.querySelector('#pickup_longitude').value), parseFloat(document.querySelector('#pickup_latitude').value)];
-    displayMap(start, coordinates)
+  const geocoder = initGeocoder("#dropoff", "Drop-off");
+  geocoder.on("result", (event) => {
+    const coordinates = event["result"]["center"];
+    document.querySelector("#dropoff_latitude").value = coordinates[1];
+    document.querySelector("#dropoff_longitude").value = coordinates[0];
+    const start = [
+      parseFloat(document.querySelector("#pickup_longitude").value),
+      parseFloat(document.querySelector("#pickup_latitude").value),
+    ];
+    displayMap(start, coordinates);
   });
 };
 
@@ -176,46 +196,55 @@ const initFlatpickr = () => {
   flatpickr("#pickup_datetime", {
     enableTime: true,
     dateFormat: "Y-m-d H:i:S",
-    defaultDate: Date.now()
+    defaultDate: Date.now(),
   });
 };
 
 const predict = () => {
-  const form = document.querySelector('form');
+  const form = document.querySelector("form");
   if (form) {
-    form.addEventListener('submit', (e) => {
+    form.addEventListener("submit", (e) => {
       e.preventDefault();
       const data = {
-        "pickup_latitude": parseFloat(document.getElementById('pickup_latitude').value) || 40.747,
-        "pickup_longitude": parseFloat(document.getElementById('pickup_longitude').value) || -73.989,
-        "dropoff_latitude": parseFloat(document.getElementById('dropoff_latitude').value) || 40.802,
-        "dropoff_longitude": parseFloat(document.getElementById('dropoff_longitude').value) || -73.956,
-        "passenger_count": parseInt(document.getElementById('passenger_count').value) || 2,
-        "pickup_datetime": document.getElementById('pickup_datetime').value
+        pickup_latitude:
+          parseFloat(document.getElementById("pickup_latitude").value) ||
+          40.747,
+        pickup_longitude:
+          parseFloat(document.getElementById("pickup_longitude").value) ||
+          -73.989,
+        dropoff_latitude:
+          parseFloat(document.getElementById("dropoff_latitude").value) ||
+          40.802,
+        dropoff_longitude:
+          parseFloat(document.getElementById("dropoff_longitude").value) ||
+          -73.956,
+        passenger_count:
+          parseInt(document.getElementById("passenger_count").value) || 2,
+        pickup_datetime: document.getElementById("pickup_datetime").value,
       };
-      let query = []
+      let query = [];
       Object.keys(data).forEach((param) => {
-        query.push(`${param}=${data[param]}`)
-      })
-      const querystring = query.join('&')
-      const url = `${taxiFareApiUrl}?${querystring}`
-      fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById('fare').classList.remove('d-none');
-        const fareResult = document.getElementById('predicted-fare');
-        const fare = Math.round(data['fare'] * 100) / 100
-        fareResult.innerText = `$${fare}`;
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+        query.push(`${param}=${data[param]}`);
       });
+      const querystring = query.join("&");
+      const url = `${taxiFareApiUrl}?${querystring}`;
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          document.getElementById("fare").classList.remove("d-none");
+          const fareResult = document.getElementById("predicted-fare");
+          const fare = Math.round(data["fare"] * 100) / 100;
+          fareResult.innerText = `$${fare}`;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     });
   }
 };
